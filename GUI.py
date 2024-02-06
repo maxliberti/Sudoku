c514aa9b01cecba4af75fe78c51336df7804ad47
Max <tempoearlvoid@gmail.com>
2024-02-05 19:06:12 -0500

    Working with user input, now need to implement game logic

---
+++ GUI.py
@@ -0,0 +1,100 @@
+from solveSudoku import Solver
+import pygame as pg
+import os
+import sys
+import copy
+
+
+class SudokuGUI:
+    def __init__(self):
+        pg.init()
+        self.screen_size = (550, 550)
+        self.screen = pg.display.set_mode(self.screen_size)
+        pg.display.set_caption("Sudoku")
+        self.clock = pg.time.Clock()
+        self.screen.fill(pg.Color("white"))
+        self.solver = Solver()
+        self.font = pg.font.SysFont('Century Gothic', 35)
+        self.board = self.solver.generate_puzzle(self.solver.board)
+        self.og_puzzle = copy.deepcopy(self.board)
+        self.selected_pos = None
+        self.play_music()
+
+    def find_music_path(self):
+        app_folder = os.path.dirname(os.path.realpath(sys.argv[0]))
+        p = os.path.join(app_folder, "sudoku_music.ogg")
+        return p
+
+    def input(self, screen, position):
+        print(f"Input function called with position: {position}")
+        i, j = position[1], position[0]
+        flag = True
+        while flag:
+            for event in pg.event.get():
+                if event.type == pg.QUIT:
+                    return
+                if event.type == pg.KEYDOWN:
+                    if self.og_puzzle[i-1][j-1] != 0:
+                        return  # Exit if the cell is not editable
+                    if (event.key == 48):  # 0 key
+                        print("0 was pressed")
+                        self.board[i-1][j-1] = 0
+                        pg.draw.rect(screen, 'white', ((i-1) * 50 + 5, (j-1) * 50 + 10, 50 - 10, 50 - 10))
+                        pg.display.update()
+                        return
+                    if (0 < event.key - 48 < 10):  # any other valid input
+                        print("key was pressed")
+                        self.board[i-1][j-1] = event.key - 48
+                        pg.draw.rect(screen, 'white', ((i-1) * 50 + 5, (j-1) * 50 + 10, 50 - 10, 50 - 10))
+                        value = self.font.render(str(event.key - 48), True, 'black')
+                        screen.blit(value, (position[0] * 50 + 15, position[1] * 50 + 5))
+                        pg.display.update()
+                        return
+
+
+    def play_music(self):
+        pg.mixer.init()
+        song = self.find_music_path()
+        pg.mixer.music.load(song)
+        pg.mixer.music.play(-1, 0, 0)
+
+    # Creates grid
+    def draw_board(self):
+        for i in range(0, self.solver.n + 1):
+            width = 4 if i % 3 == 0 else 2
+            pg.draw.line(self.screen, (0, 0, 0), (50 + 50 * i, 50), (50 + 50 * i, 500), width)
+            pg.draw.line(self.screen, (0, 0, 0), (50, 50 + 50 * i), (500, 50 + 50 * i), width)
+        pg.display.update()
+
+    def print_values(self, grid):
+        for i in range(self.solver.n):
+            for j in range(self.solver.n):
+                if grid[i][j] != 0:
+                    # render number to print
+                    value = self.font.render(str(grid[i][j]), True, 'black')
+                    # print it to screen at x, y coordinate
+                    self.screen.blit(value, ((j + 1) * 50 + 15, (i + 1) * 50 + 5))
+        pg.display.update()
+
+    def main(self):
+        running = True
+        while running:
+            for event in pg.event.get():
+                if event.type == pg.MOUSEBUTTONUP and event.button == 1:
+                    pos = pg.mouse.get_pos()
+                    self.selected_pos = (pos[0] // 50, pos[1] // 50)
+                    self.input(self.screen, self.selected_pos)
+                if event.type == pg.QUIT:
+                    running = False
+
+            self.draw_board()
+            self.print_values(self.board)
+            # self.print_values()
+            pg.display.flip()
+            self.clock.tick(60)
+        pg.quit()
+
+
+if __name__ == '__main__':
+    sudokuGUI = SudokuGUI()
+    sudokuGUI.main()
---
+++ solveSudoku.py
@@ -0,0 +1,96 @@
+import random
+
+import numpy as np
+from random import sample
+from random import shuffle
+
+
+class Solver:
+    def __init__(self):
+        # size of the 2d matrix/sudoku board
+        self.n = 9
+
+        self.board = [
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0]
+        ]
+
+    def debug(self):
+        print(np.matrix(self.board))
+
+    def generate_puzzle(self, grid):
+        self.solve(grid)
+        squares = self.n*self.n
+        remove = squares * 3 // 4
+        for i in sample(range(squares), remove):
+            grid[i//self.n][i%self.n] = 0
+        return grid
+
+
+    def solve(self, grid, row = 0, col = 0):
+        # if row value is such that the function has run its course, break
+        if row == self.n:
+            return True
+        # if on the last cell of the column, move to the next row
+        elif col == self.n:
+            return self.solve(grid, row + 1, 0)
+        # if at some other coordinate, move to the next cell
+        elif grid[row][col] != 0:
+            return self.solve(grid, row, col + 1)
+        # if the cell is empty
+        else:
+            nums = list(range(1,10))
+            random.shuffle(nums)
+            # for every possible value a cell can hold 1-9
+            for num in nums:
+                # if that value is valid at the position, place it
+                if self.valid(grid, row, col, num):
+                    # print(np.matrix(board))
+                    grid[row][col] = num
+                    # if placing number at this cell eventually leads to a solution return true
+                    if self.solve(grid, row, col + 1):
+                        return True
+                    # else empty the cell and try something else on next iteration
+                    grid[row][col] = 0
+            # no solution
+            return False
+
+
+    # function to check if 1 specific square (grid[row][col]) can contain value num legit
+    def valid(self, grid, row, col, num):
+        in_row = num in grid[row]
+        in_column = num in [grid[r][col] for r in range(self.n)]
+        if in_row or in_column:
+            return False
+        # get first cells in subgrid
+        row_first_cell, column_first_cell = self.get_subgrid_starting_cells(row, col)
+        # get last cells
+        row_last_cell = row_first_cell + 3
+        column_last_cell = column_first_cell + 3
+        for r in range(row_first_cell, row_last_cell):
+            for c in range(column_first_cell, column_last_cell):
+                # if num is already in subgrid return false
+                if grid[r][c] == num:
+                    return False
+        return True
+
+
+    def get_subgrid_starting_cells(self, row, col):
+        return row // 3 * 3, col // 3 * 3
+
+solver = Solver()
+# solver.debug()
+# solver.solve(solver.board)
+# solver.debug()
+
+
+
+
+
